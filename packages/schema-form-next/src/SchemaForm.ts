54debb52ed16648d698490d34f7d3ec4c5aa0008
ckangwen <2437037469@qq.com>
2023-01-03 09:50:25 +0800

    feat(schema-form-next): 增加emit类型提示 & 继承el-form的props和events & 增加extra插槽

--- packages/schema-form-next/src/SchemaForm.ts
+++ packages/schema-form-next/src/SchemaForm.ts
@@ -1,7 +1,7 @@
-import { h, ref, watch, defineComponent, PropType, computed, Slots } from "vue";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { h, ref, watch, defineComponent, computed, Slots } from "vue";
 import { ElForm } from "element-plus";
 import { CharrueSchemaField } from "./SchemaField";
-import { FormSchemaDef } from "./types/public";
 import {
   isEqual,
   has,
@@ -9,37 +9,25 @@ import {
   FORM_ITEM_SLOT_NAMES,
   FORM_ITEM_SLOT_PREFIX,
 } from "./utils";
-import type { FormRules } from "element-plus";
+import type { FormRules, FormItemProp } from "element-plus";
+import { schemaFormProps } from "./props";
 
 type SchemaFormData = Record<string, any>;
 
 export const CharrueSchemaForm = defineComponent({
   name: "CharrueSchemaForm",
-  props: {
-    modelValue: {
-      type: Object,
-      default() {
-        return {};
-      },
-    },
-    schema: {
-      type: Object as PropType<Record<string, FormSchemaDef>>,
-      required: true,
-    },
-    rules: {
-      type: Object as PropType<FormRules>,
-      default() {
-        return {};
-      },
-    },
+  props: schemaFormProps,
+  emits: {
+    validate: (prop: FormItemProp, isValid: boolean, message: string) => true,
+    "update:modelValue": (value: SchemaFormData) => true,
   },
-  emits: ["update:modelValue"],
   setup(props, { emit, slots, expose }) {
     const fields = computed(() => {
       return Object.keys(props.schema || {});
     });
     const elFormRef = ref();
 
+    /** 表单字段插槽 */
     const fieldSlots = computed(() => {
       const currentSlots: Record<string, Record<string, Slots[string]>> = {};
       Object.keys(slots).forEach((slotName) => {
@@ -70,6 +58,7 @@ export const CharrueSchemaForm = defineComponent({
       return currentSlots;
     });
 
+    /** rule + required */
     const computedRules = computed(() => {
       const { rules, schema } = props;
       return fields.value.reduce((acc, fieldProp) => {
@@ -79,7 +68,7 @@ export const CharrueSchemaForm = defineComponent({
           acc[fieldProp] = [
             {
               required: true,
-              message: `${schema?.[fieldProp].title}不能为空`,
+              message: `${schema?.[fieldProp].label}不能为空`,
               trigger: "blur",
             },
           ];
@@ -131,13 +120,20 @@ export const CharrueSchemaForm = defineComponent({
     const onInput = (key: string, value: any) => {
       formData.value[key] = value;
     };
+    const onValidate = (
+      prop: FormItemProp,
+      isValid: boolean,
+      message: string
+    ) => {
+      emit("validate", prop, isValid, message);
+    };
 
     expose({
-      validate: () => elFormRef.value?.validate,
-      scrollToField: () => elFormRef.value?.scrollToField,
-      clearValidate: () => elFormRef.value?.clearValidate,
-      validateField: () => elFormRef.value?.validateField,
-      resetFields: () => elFormRef.value?.resetFields,
+      validate: () => elFormRef.value?.validate?.(),
+      scrollToField: () => elFormRef.value?.scrollToField?.(),
+      clearValidate: () => elFormRef.value?.clearValidate?.(),
+      validateField: () => elFormRef.value?.validateField?.(),
+      resetFields: () => elFormRef.value?.resetFields?.(),
     });
 
     return {
@@ -148,41 +144,45 @@ export const CharrueSchemaForm = defineComponent({
       fieldSlots,
 
       onInput,
+      onValidate,
     };
   },
   render() {
-    const {
-      $attrs,
-      schema,
-      fields,
-      formData,
-      computedRules,
-      fieldSlots,
-
-      onInput,
-    } = this;
     return h(
-      ElForm as any,
+      ElForm,
       {
         class: "charrue-schema-form-container",
-        model: formData,
-        rules: computedRules,
         ref: "elFormRef",
-        ...$attrs,
+        model: this.formData,
+        rules: this.computedRules,
+        inline: this.inline,
+        labelPosition: this.labelPosition,
+        labelWidth: this.labelWidth,
+        labelSuffix: this.labelSuffix,
+        size: this.size,
+        showMessage: this.showMessage,
+        inlineMessage: this.inlineMessage,
+        statusIcon: this.statusIcon,
+        validateOnRuleChange: this.validateOnRuleChange,
+        hideRequiredAsterisk: this.hideRequiredAsterisk,
+        disabled: this.disabled,
+        onValidate: this.onValidate,
       },
-      () =>
-        fields.map((fieldProp, index) => {
+      () => [
+        ...this.fields.map((fieldProp, index) => {
           return h(
             CharrueSchemaField,
             {
               key: `schema-field-${fieldProp}-${index}`,
-              schema: schema[fieldProp],
-              value: formData[fieldProp],
-              "onUpdate:value": (value: any) => onInput(fieldProp, value),
+              schema: this.schema[fieldProp],
+              value: this.formData[fieldProp],
+              "onUpdate:value": (value: any) => this.onInput(fieldProp, value),
             } as any,
-            fieldSlots[fieldProp]
+            this.fieldSlots[fieldProp]
           );
-        })
+        }),
+        this.$slots.extra ? this.$slots.extra() : [],
+      ]
     );
   },
 });
---
+++ packages/schema-form-next/src/props.ts
@@ -0,0 +1,80 @@
+import { PropType } from "vue";
+import { ElForm } from "element-plus";
+import type { FormRules } from "element-plus";
+import type { FormSchemaDef } from "./types/public";
+
+type ElFormProps = InstanceType<typeof ElForm>["$props"];
+
+export const schemaFormProps = {
+  modelValue: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+  schema: {
+    type: Object as PropType<Record<string, FormSchemaDef>>,
+    default() {
+      return {};
+    },
+    required: true,
+  },
+  rules: {
+    type: Object as PropType<FormRules>,
+    default() {
+      return {};
+    },
+  },
+  inline: {
+    type: Boolean,
+    default: false,
+  },
+  labelPosition: {
+    type: String as PropType<ElFormProps["labelPosition"]>,
+    default: "right",
+  },
+  labelWidth: {
+    type: String as PropType<ElFormProps["labelWidth"]>,
+    default: "100px",
+  },
+  labelSuffix: {
+    type: String as PropType<ElFormProps["labelSuffix"]>,
+    default: "",
+  },
+  hideRequiredAsterisk: {
+    type: Boolean as PropType<ElFormProps["hideRequiredAsterisk"]>,
+    default: false,
+  },
+  requireAsteriskPosition: {
+    type: String as PropType<ElFormProps["requireAsteriskPosition"]>,
+    default: "left",
+  },
+  showMessage: {
+    type: Boolean as PropType<ElFormProps["showMessage"]>,
+    default: true,
+  },
+  inlineMessage: {
+    type: Boolean as PropType<ElFormProps["inlineMessage"]>,
+    default: false,
+  },
+  statusIcon: {
+    type: Boolean as PropType<ElFormProps["statusIcon"]>,
+    default: false,
+  },
+  validateOnRuleChange: {
+    type: Boolean as PropType<ElFormProps["validateOnRuleChange"]>,
+    default: true,
+  },
+  size: {
+    type: String as PropType<ElFormProps["size"]>,
+    default: "default",
+  },
+  disabled: {
+    type: Boolean as PropType<ElFormProps["disabled"]>,
+    default: false,
+  },
+  scrollToError: {
+    type: Boolean as PropType<ElFormProps["scrollToError"]>,
+    default: false,
+  },
+};
